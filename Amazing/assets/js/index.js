da06f90949bdb8991e67bb1dca007a04f9ae0ce5
ProDeveloper-1 <prodeveloper-1@localhost.localdomain>
2020-05-20 19:13:55 -0400

    More changes

--- Amazing/assets/js/index.js
+++ Amazing/assets/js/index.js
@@ -1,75 +1,75 @@
-// // Change Images
-// // Boys
-// function changeImage() {
-// 	document.getElementById('boys').src = "assets/img/boys2.png";
-// }
+// Change Images
+// Boys
+function changeImage() {
+	document.getElementById('boys').src = "assets/img/boys2.png";
+}
 
-// function defaultImage() {
-// 	document.getElementById('boys').src = "assets/img/boys1.png";
-// }
+function defaultImage() {
+	document.getElementById('boys').src = "assets/img/boys1.png";
+}
 
-// // Girls
-// function changeImage1() {
-// 	document.getElementById('girls').src = "assets/img/girls-2.png";
-// }
+// Girls
+function changeImage1() {
+	document.getElementById('girls').src = "assets/img/girls-2.png";
+}
 
-// function defaultImage1() {
-// 	document.getElementById('girls').src = "assets/img/girls-1.png";
-// }
+function defaultImage1() {
+	document.getElementById('girls').src = "assets/img/girls-1.png";
+}
 
-// // Men
-// function changeImage2() {
-// 	document.getElementById('men').src = "assets/img/trouser-1.jpg";
-// }
+// Men
+function changeImage2() {
+	document.getElementById('men').src = "assets/img/trouser-1.jpg";
+}
 
-// function defaultImage2() {
-// 	document.getElementById('men').src = "assets/img/trouser.jpg";
-// }
+function defaultImage2() {
+	document.getElementById('men').src = "assets/img/trouser.jpg";
+}
 
-// // Women
-// function changeImage3() {
-// 	document.getElementById('women').src = "assets/img/women2.jpg";
-// }
+// Women
+function changeImage3() {
+	document.getElementById('women').src = "assets/img/women2.jpg";
+}
 
-// function defaultImage3() {
-// 	document.getElementById('women').src = "assets/img/women1.jpg";
-// }
-
-
-// // Sneakers
-// function changeImage4() {
-// 	document.getElementById('sneakers').src = "assets/img/sneakers.jpg";
-// }
-
-// function defaultImage4() {
-// 	document.getElementById('sneakers').src = "assets/img/sneakers1.jpg";
-// }
-
-// // Boys & Girls
-// function changeImage5() {
-// 	document.getElementById('BG').src = "assets/img/girls.png";
-// }
-
-// function defaultImage5() {
-// 	document.getElementById('BG').src = "assets/img/boys.png";
-// }
+function defaultImage3() {
+	document.getElementById('women').src = "assets/img/women1.jpg";
+}
 
 
-// // Women's bags
-// function changeImage6() {
-// 	document.getElementById('bags').src = "assets/img/bag2.jpg";
-// }
+// Sneakers
+function changeImage4() {
+	document.getElementById('sneakers').src = "assets/img/sneakers.jpg";
+}
 
-// function defaultImage6() {
-// 	document.getElementById('bags').src = "assets/img/bag1.jpg";
-// }
+function defaultImage4() {
+	document.getElementById('sneakers').src = "assets/img/sneakers1.jpg";
+}
+
+// Boys & Girls
+function changeImage5() {
+	document.getElementById('BG').src = "assets/img/girls.png";
+}
+
+function defaultImage5() {
+	document.getElementById('BG').src = "assets/img/boys.png";
+}
 
 
-// // W-tops
-// function changeImage7() {
-// 	document.getElementById('W-tops').src = "assets/img/w-top.png";
-// }
+// Women's bags
+function changeImage6() {
+	document.getElementById('bags').src = "assets/img/bag2.jpg";
+}
 
-// function defaultImage7() {
-// 	document.getElementById('W-tops').src = "assets/img/w-top2.png";
-// }
+function defaultImage6() {
+	document.getElementById('bags').src = "assets/img/bag1.jpg";
+}
+
+
+// W-tops
+function changeImage7() {
+	document.getElementById('W-tops').src = "assets/img/w-top.png";
+}
+
+function defaultImage7() {
+	document.getElementById('W-tops').src = "assets/img/w-top2.png";
+}
