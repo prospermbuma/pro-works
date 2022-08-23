6f49aa0e92b7924cd689b4f630cecf16cc8f004a
ProDeveloper-1 <prospermbuma@gmail.com>
2022-08-23 11:40:51 +0300

    Changes

--- celebrate/assets/js/mc_user_view.js
+++ celebrate/assets/js/mc_user_view.js
@@ -6,7 +6,7 @@ const pass_word = document.querySelector('#sign_up .user-account #pswd_1');
 const pass_word_2 = document.querySelector('#sign_up .user-account #pswd_2');
 
 const pswd_eye = document.querySelector('#sign_up .user-account .pswd-1 .fa-eye');
-pswd_eye.addEventListener('click', function () {
+pswd_eye.addEventListener('click', function() {
     pswd_eye.classList.add('hide');
     pswd_eye_slash.classList.add('show');
     if (pass_word.getAttribute("type") === "password") {
@@ -15,7 +15,7 @@ pswd_eye.addEventListener('click', function () {
 })
 
 const pswd_eye_slash = document.querySelector('#sign_up .user-account .pswd-1 .fa-eye-slash');
-pswd_eye_slash.addEventListener('click', function () {
+pswd_eye_slash.addEventListener('click', function() {
     pswd_eye_slash.classList.remove('show');
     pswd_eye.classList.remove('hide');
     if (pass_word.getAttribute("type") === "text") {
@@ -24,7 +24,7 @@ pswd_eye_slash.addEventListener('click', function () {
 })
 
 const pswd_eye_2 = document.querySelector('#sign_up .user-account .pswd-2 .fa-eye');
-pswd_eye_2.addEventListener('click', function () {
+pswd_eye_2.addEventListener('click', function() {
     pswd_eye_2.classList.add('hide');
     pswd_eye_slash_2.classList.add('show');
     if (pass_word_2.getAttribute("type") === "password") {
@@ -33,7 +33,7 @@ pswd_eye_2.addEventListener('click', function () {
 })
 
 const pswd_eye_slash_2 = document.querySelector('#sign_up .user-account .pswd-2 .fa-eye-slash');
-pswd_eye_slash_2.addEventListener('click', function () {
+pswd_eye_slash_2.addEventListener('click', function() {
     pswd_eye_slash_2.classList.remove('show');
     pswd_eye_2.classList.remove('hide');
     if (pass_word_2.getAttribute("type") === "text") {
@@ -46,7 +46,7 @@ const pass_word_user1 = document.querySelector('#sign_up .user-account-2 #pswd_1
 const pass_word_user2 = document.querySelector('#sign_up .user-account-2 #pswd_2');
 
 const pass_eye = document.querySelector('#sign_up .user-account-2 .pswd-1 .fa-eye');
-pass_eye.addEventListener('click', function () {
+pass_eye.addEventListener('click', function() {
     pass_eye.classList.add('hide');
     pass_eye_slash.classList.add('show');
     if (pass_word_user1.getAttribute("type") === "password") {
@@ -55,7 +55,7 @@ pass_eye.addEventListener('click', function () {
 })
 
 const pass_eye_slash = document.querySelector('#sign_up .user-account-2 .pswd-1 .fa-eye-slash');
-pass_eye_slash.addEventListener('click', function () {
+pass_eye_slash.addEventListener('click', function() {
     pass_eye_slash.classList.remove('show');
     pass_eye.classList.remove('hide');
     if (pass_word_user1.getAttribute("type") === "text") {
@@ -64,7 +64,7 @@ pass_eye_slash.addEventListener('click', function () {
 })
 
 const pass_eye_2 = document.querySelector('#sign_up .user-account-2 .pswd-2 .fa-eye');
-pass_eye_2.addEventListener('click', function () {
+pass_eye_2.addEventListener('click', function() {
     pass_eye_2.classList.add('hide');
     pass_eye_slash_2.classList.add('show');
     if (pass_word_user2.getAttribute("type") === "password") {
@@ -73,10 +73,28 @@ pass_eye_2.addEventListener('click', function () {
 })
 
 const pass_eye_slash_2 = document.querySelector('#sign_up .user-account-2 .pswd-2 .fa-eye-slash');
-pass_eye_slash_2.addEventListener('click', function () {
+pass_eye_slash_2.addEventListener('click', function() {
     pass_eye_slash_2.classList.remove('show');
     pass_eye_2.classList.remove('hide');
     if (pass_word_user2.getAttribute("type") === "text") {
         pass_word_user2.setAttribute("type", "password");
     }
 })
+
+/* ==================================================
+# Video
+===================================================*/
+
+const video = document.querySelector("video");
+
+function openFullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) { /* Firefox */
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) { /* IE/Edge */
+        video.msRequestFullscreen();
+    }
+}
