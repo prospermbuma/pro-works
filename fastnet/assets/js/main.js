f1b81382cd1a82f8621b8ddda677017c215efe65
ProDeveloper-1 <prospermbuma@gmail.com>
2023-03-12 22:02:28 +0300

    More changes

--- fastnet/assets/js/main.js
+++ fastnet/assets/js/main.js
@@ -33,3 +33,23 @@ window.addEventListener('scroll', () => {
         goTop.classList.remove('active');
     }
 });
+
+/* ==================================================
+# Initialize Swiper
+===================================================*/
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
