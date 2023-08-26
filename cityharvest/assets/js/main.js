a61e6a8c35ab48e20750ab4e56600f784b4bad0e
ProDeveloper-1 <prospermbuma@gmail.com>
2023-08-26 03:14:54 +0300

    more

--- cityharvest/assets/js/main.js
+++ cityharvest/assets/js/main.js
@@ -12,17 +12,25 @@ AOS.init({
 # Navigation
 ===================================================== */
 const navbar = document.querySelector(".navbar");
+const navbar_menu = document.querySelector(".navbar-menu");
 const nav_brand = document.querySelector(".navbar #logo");
 const nav_link_change = document.querySelectorAll(".navbar .change-Color");
 const nav_btn_login = document.querySelector(".login");
 const nav_btn_give = document.querySelector(".give");
+// Define the media query
+const mediaQuery = window.matchMedia('(max-width: 768px)');
 
 window.addEventListener('scroll', () => {
    if (window.scrollY > 50) {
-      navbar.style.background = "hsl(0, 0%, 100%, .9)";
+      navbar.style.background = "hsla(0, 0%, 100%, .9)";
       navbar.style.boxShadow = "0 3px 10px rgba(0, 0, 0, 0.1)";
       navbar.style.backdropFilter = "blur(10px)";
       navbar.style.top = "0";
+      // Check if the media query is true
+      if (mediaQuery.matches) {
+         navbar_menu.style.background = "#f8f9fa";
+         navbar_menu.style.backdropFilter = "blur(0px)";
+      }
       nav_brand.src = "assets/img/logo/logo3.png";
       nav_link_change.forEach(link => {
          link.style.color = "#000";
@@ -55,6 +63,11 @@ window.addEventListener('scroll', () => {
       navbar.style.boxShadow = "none";
       navbar.style.backdropFilter = "none";
       navbar.style.top = "22px";
+      // Check if the media query is true
+      if (mediaQuery.matches) {
+         navbar_menu.style.background = "hsla(120, 1%, 19%, 0.9)";
+         navbar_menu.style.backdropFilter = "blur(10px)";
+      }
       nav_brand.src = "assets/img/logo/logo2.png";
       nav_link_change.forEach(link => {
          link.style.color = "#f8f9fa";
