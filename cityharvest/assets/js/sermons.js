cbc8868d7b69bfb906b79a7bb0c02ec0f7e73b00
ProDeveloper-1 <prospermbuma@gmail.com>
2023-09-06 07:29:36 +0300

    Changes on API

--- cityharvest/assets/js/sermons.js
+++ cityharvest/assets/js/sermons.js
@@ -96,6 +96,8 @@ const API_KEY = 'AIzaSyDeHAzKbI8wQ3hwhzhf_7tMnmkzZCQYliY';
 const PLAYLIST_ID = 'UUQLLAyIDCHSstiXWt5w2dpQ';
 const MAX_RESULTS = 6; // Number of videos per page.
 let nextPageToken = '';
+let prevPageToken = '';
+// let pageTokens = []; // Stack to store page tokens
 let currentPage = 1;
 let totalVideos = 0;
 
@@ -113,11 +115,16 @@ function start() {
             playlistId: PLAYLIST_ID,
             maxResults: MAX_RESULTS,
             pageToken: nextPageToken,
+            // pageToken: pageTokens.length > 0 ? pageTokens[pageTokens.length - 1] : '', // Use the top page token
+            order: 'date', // Order by date
+            sort: 'asc', // Sort in ascending order (oldest first)
+            // For descending order (newest first), you can use 'desc'.
+            // The default order is 'desc'.
+            // sort: 'desc',
          },
       });
    }).then(response => {
       const videoList = document.getElementById('video-list');
-      const pagination = document.getElementById('pagination');
       const videos = response.result.items;
 
       if (videos.length === 0) {
@@ -127,6 +134,7 @@ function start() {
 
       totalVideos = response.result.pageInfo.totalResults;
       nextPageToken = response.result.nextPageToken;
+      prevPageToken = response.result.prevPageToken;
 
       videos.forEach(video => {
          const title = video.snippet.title;
@@ -138,7 +146,7 @@ function start() {
          videoDiv.setAttribute('data-aos', 'fade-up');// Set AOS attributes
          videoDiv.setAttribute('data-aos-delay', '100');// Set AOS attributes
          videoDiv.innerHTML = `
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}?showinfo=0" frameborder="0" allowfullscreen>
+          <iframe src="https://www.youtube.com/embed/${videoId}?controls=0&showinfo=0" frameborder="0" allowfullscreen>
           </iframe>
           <h2 class="video-title">${title}</h2>
         `;
@@ -146,13 +154,19 @@ function start() {
          videoList.appendChild(videoDiv);
       });
 
+      updatePageDisplay();
       updatePagination();
    });
 }
 
+function updatePageDisplay() {
+   const totalPages = Math.ceil(totalVideos / MAX_RESULTS);
+   const pageDisplay = document.getElementById('page-number');
+   pageDisplay.innerText = `Page ${currentPage} of ${totalPages}`;
+}
+
 function updatePagination() {
    const totalPages = Math.ceil(totalVideos / MAX_RESULTS);
-
    const pagination = document.getElementById('pagination');
    pagination.innerHTML = '';
 
@@ -160,6 +174,11 @@ function updatePagination() {
       const prevButton = createPaginationButton('Prev', currentPage - 1, 'prev');
       pagination.appendChild(prevButton);
    }
+   
+   // for (let i = 1; i <= 3; i++) {
+   //    const pageButton = createPaginationButton(i, i, 'pagination-button');
+   //    pagination.appendChild(pageButton);
+   // }
 
    if (currentPage < totalPages) {
       const nextButton = createPaginationButton('Next', currentPage + 1, 'next');
@@ -175,13 +194,48 @@ function createPaginationButton(label, page, className = '') {
    return button;
 }
 
+// function goToPage(page) {
+//    if (page === 'prev' && pageTokens.length > 1) {
+//       pageTokens.pop(); // Remove the current page token
+//       currentPage--;
+//    } else if (page === 'next') {
+//       pageTokens.push(pageTokens[pageTokens.length - 1]); // Push the current page token
+//       currentPage++;
+//    }
+//    else {
+//       // A specific page number was clicked
+//       pageTokens.push(pageTokens[pageTokens.length - 1]); // Push the current page token
+//       currentPage = page;
+//    }
+
 function goToPage(page) {
    currentPage = page;
    const startIndex = (currentPage - 1) * MAX_RESULTS;
    const endIndex = startIndex + MAX_RESULTS;
 
+   // const startIndex = (currentPage - 1) * MAX_RESULTS;
+   // const endIndex = startIndex + MAX_RESULTS;
+
    const videoList = document.getElementById('video-list');
    videoList.innerHTML = '';
 
+   if (page === 1) {
+      nextPageToken = ''; // Reset nextPageToken when going to the first page
+   }
+
+   // if (page === Math.ceil(totalVideos / MAX_RESULTS)) {
+   //    //If on the last page, do not set nextPageToken
+   // } else {
+   //    nextPageToken = ''; // Reset nextPageToken when going to any other page
+   // }
+
+   if (page > 1) {
+      prevPageToken = ''; // Reset prevPageToken when not on the first page
+   }
+
    start(); // Fetch and display videos for the new page
 }
+
+// Initial load
+start();
+
